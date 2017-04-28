c1399e8e4214334c19548ddb2f80187d0844a4fc
JoelH05 <j_h_olla@hotmail.com>
2017-04-28 14:06:10 -0400

    Completed lab

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -6,9 +6,24 @@
 //  Copyright © 2016 Flatiron School. All rights reserved.
 //
 
+protocol ProvideAccess {
+    func allowEntryWithPassword(password: [Int]) -> Bool
+    
+}
+
+
+extension BankVault {
+    func allowEntryWithPassword(_ password: [Int]) -> Bool {
+        if password.isEmpty || password.count > 10 { return false }
+        for (i, num) in password.enumerated() {
+            if i % 2 == 0 && num % 2 != 0 { return false }
+        }
+        return true
+    }
+}
 
 class BankVault {
-    
+
     let name: String
     let address: String
     var amount: Double = 0.0
